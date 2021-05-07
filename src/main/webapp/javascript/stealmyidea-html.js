2f2615a67c135642d9a0d31ea352ba03a4b26f3a
Al Bundy <albundy@bundy.com>
2021-05-06 20:18:03 -0400

    Blah

--- src/main/webapp/javascript/stealmyidea-html.js
+++ src/main/webapp/javascript/stealmyidea-html.js
@@ -57,10 +57,14 @@ function createIdeaHtml(idea){
 							'<span class="idea-common-label idea-date-label">Date:</span>' +
 							'<span class="idea-common-content idea-date-content">' + nullToEmpty(idea.ideaDate) + '</span>' + 
 						'</div>' +
-						'<div id="idea-steal-status-description-container-' + idea.ideaId + '" class="idea-common-container idea-steal-status-description-container">' + 
-							'<span class="idea-common-label idea-steal-status-description-label">Has it been stolen?</span>' +
-							'<span class="idea-common-content idea-steal-status-description-content">' + nullToEmpty(idea.stealStatusDescription) + '</span>' + 
+						'<div id="idea-steal-status-container-' + idea.ideaId + '" class="idea-common-container idea-steal-status-container">' + 
+							'<span class="idea-common-label idea-steal-status-label">Has it been stolen?</span>' +
+							'<span class="idea-common-content idea-steal-status-content">' + nullToEmpty(idea.stealStatus) + '</span>' + 
 						'</div>' +
+						'<div id="idea-steal-status-description-container-' + idea.ideaId + '" class="idea-common-container idea-steal-status-description-container">' + 
+						'<span class="idea-common-label idea-steal-status-description-label">Who stole it?</span>' +
+						'<span class="idea-common-content idea-steal-status-description-content">' + nullToEmpty(idea.stealStatusDescription) + '</span>' + 
+					'</div>' +
 						'<div id="idea-greatness-container-' + idea.ideaId + '" class="idea-common-container idea-greatness-container">' + 
 							'<span class="idea-common-label idea-greatness-label">Greatness:</span>' +
 							'<span class="idea-common-content idea-greatness-content">' + nullToEmpty(idea.greatness) + '</span>' + 
